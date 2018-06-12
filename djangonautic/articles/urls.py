91a13b7ab684fe83249b9e99a7c7a5c10f519513
David-J-89 <davidwhaleycode@outlook.com>
2018-06-12 08:27:31 -0500

    blog-03

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .import views
+from django.urls import include, re_path
 
 urlpatterns = [
-    path('', views.article_list),
+    path(r'', views.article_list, name="list"),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail)
 ]
