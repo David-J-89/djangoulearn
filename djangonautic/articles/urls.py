845bfd5ca432bc91761e78e3f35e4c050dbcc67b
David-J-89 <davidwhaleycode@outlook.com>
2018-06-13 14:23:46 -0500

    fixed redirect issue

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .import views
 from django.urls import include, re_path
 
+app_name='articles'
+
 urlpatterns = [
     path(r'', views.article_list, name="list"),
     re_path(r'(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
