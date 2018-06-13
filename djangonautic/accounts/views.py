b5379e7f270bf0b23cae844f574d13e8778bc781
David-J-89 <davidwhaleycode@outlook.com>
2018-06-13 14:53:13 -0500

    adds

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -12,3 +12,12 @@ def signup_view(request):
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', { 'form': form })
 
+def login_view(request): 
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log in the user
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form':form})
