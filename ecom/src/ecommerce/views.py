139acbdb2ac70fabd8127154763228b94ddf5787
David-J-89 <davidwhaleycode@outlook.com>
2018-06-18 14:21:22 -0500

    next1

--- ecom/src/ecommerce/views.py
+++ ecom/src/ecommerce/views.py
@@ -5,6 +5,14 @@ from django.shortcuts import render
 def home_page(request):
   return render(request, "home_page.html", {})
 
+def about_page(request):
+  return render(request, "home_page.html", {})
+
+def contact_page(request):
+  return render(request, "home_page.html", {})
+
+
+
 #def home_page_old(request):
  #   html_ ="""
   #  <!doctype html>
