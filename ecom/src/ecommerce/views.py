9b747cd96e2dbbcbd101832de84cd3504aab045f
David-J-89 <davidwhaleycode@outlook.com>
2018-06-18 15:13:25 -0500

    next2

--- ecom/src/ecommerce/views.py
+++ ecom/src/ecommerce/views.py
@@ -3,13 +3,25 @@ from django.shortcuts import render
 
 
 def home_page(request):
-  return render(request, "home_page.html", {})
+  context = {
+    "title":"Hello World!",
+    "content": "Welcome to the home page."
+  }
+  return render(request, "home_page.html", context)
 
 def about_page(request):
-  return render(request, "home_page.html", {})
+  context = {
+    "title":"About Page",
+    "content": " Welcome to the about page."
+  }
+  return render(request, "home_page.html", context)
 
 def contact_page(request):
-  return render(request, "home_page.html", {})
+  context = {
+    "title":"Contact",
+    "content": "welcome to the contact page."
+  }
+  return render(request, "contact/view.html", context)
 
 
 
