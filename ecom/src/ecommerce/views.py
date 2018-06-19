d4536df230659ce7ae580c755f9a70591da9535f
David-J-89 <davidwhaleycode@outlook.com>
2018-06-19 13:13:45 -0500

    format some things

--- ecom/src/ecommerce/views.py
+++ ecom/src/ecommerce/views.py
@@ -1,58 +1,68 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 
 def home_page(request):
-  context = {
-    "title":"Hello World!",
-    "content": "Welcome to the home page."
-  }
-  return render(request, "home_page.html", context)
+    context = {
+        "title": "Hello World!",
+        "content": "Welcome to the home page."
+    }
+    return render(request, "home_page.html", context)
+
 
 def about_page(request):
-  context = {
-    "title":"About Page",
-    "content": " Welcome to the about page."
-  }
-  return render(request, "home_page.html", context)
+    context = {
+        "title": "About Page",
+        "content": " Welcome to the about page."
+    }
+    return render(request, "home_page.html", context)
+
 
 def contact_page(request):
-  context = {
-    "title":"Contact",
-    "content": "welcome to the contact page."
-  }
-  return render(request, "contact/view.html", context)
+    contact_form = ContactForm()
+    context = {
+        "title": "Contact",
+        "content": "welcome to the contact page.",
+        "form": contact_form
+    }
+    if request.method == "POST":
+        print(request.POST)
+        print(request.POST.get('fullname'))
+        print(request.POST.get('email'))
+        print(request.POST.get('content'))
+    return render(request, "contact/view.html", context)
 
 
+# def home_page_old(request):
+   #   html_ ="""
+    #  <!doctype html>
+# <html lang="en">
 
-#def home_page_old(request):
- #   html_ ="""
-  #  <!doctype html>
-#<html lang="en">
+# <head>
+   #   <meta charset="utf-8">
+    #  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    # <!-- Bootstrap core CSS -->
+    # <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+# </head>
 
-#<head>
- #   <meta charset="utf-8">
-  #  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-   # <!-- Bootstrap core CSS -->
-   # <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-#</head>
+# <body>
 
-#<body>
+   #   <div class='text-center'>
+    #      <h1>Hello, world!</h1>
+    # </div>
 
- #   <div class='text-center'>
-  #      <h1>Hello, world!</h1>
-   # </div>
-
-   # <!-- Bootstrap core JavaScript
-   #  ================================================== -->
-   # <!-- Placed at the end of the document so the pages load faster -->
-   # <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    # <!-- Bootstrap core JavaScript
+    #  ================================================== -->
+    # <!-- Placed at the end of the document so the pages load faster -->
+    # <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
     #    crossorigin="anonymous"></script>
-    #<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    #<script src="../../assets/js/vendor/popper.min.js"></script>
-    #<script src="../../dist/js/bootstrap.min.js"></script>
-#</body>
+    # <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    # <script src="../../assets/js/vendor/popper.min.js"></script>
+    # <script src="../../dist/js/bootstrap.min.js"></script>
+# </body>
 
-#</html>
-#"""
- #   return HttpResponse(html_)
+# </html>
+# """
+   #   return HttpResponse(html_)
