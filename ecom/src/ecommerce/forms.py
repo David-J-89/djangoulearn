7e47758b754a9d4af0deed480d2d4c4044508753
David-J-89 <davidwhaleycode@outlook.com>
2018-06-20 10:18:26 -0500

    newday

--- ecom/src/ecommerce/forms.py
+++ ecom/src/ecommerce/forms.py
@@ -10,5 +10,28 @@ class ContactForm(forms.Form):
             }
         )
     )
-    email = forms.EmailField()
-    content = forms.CharField()
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control", "placeholder": "Your email"
+            }
+        )
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                "placeholder": "Your email"
+            }
+        )
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("Email has to be gmail.com")
+        return email
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
--- ecom/src/ecommerce/views.py
+++ ecom/src/ecommerce/views.py
@@ -21,46 +21,54 @@ def about_page(request):
 
 
 def contact_page(request):
-    contact_form = ContactForm()
+    contact_form = ContactForm(request.POST or None)
     context = {
         "title": "Contact",
         "content": "welcome to the contact page.",
         "form": contact_form
     }
-    if request.method == "POST":
-        print(request.POST)
-        print(request.POST.get('fullname'))
-        print(request.POST.get('email'))
-        print(request.POST.get('content'))
+    if contact_form.is_valid():
+      print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     #print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
     return render(request, "contact/view.html", context)
 
+def login_page(request):
+    return render(request, "auth/login.html", {})
 
-# def home_page_old(request):
-   #   html_ ="""
-    #  <!doctype html>
+def register_page(request):
+    return render(request, "auth/register.html", {})
+
+
+def home_page_old(request):
+      html_ ="""
+#      <!doctype html>
 # <html lang="en">
 
 # <head>
-   #   <meta charset="utf-8">
-    #  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    # <!-- Bootstrap core CSS -->
-    # <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+#      <meta charset="utf-8">
+#      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+#     <!-- Bootstrap core CSS -->
+#     <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
 # </head>
 
 # <body>
 
-   #   <div class='text-center'>
-    #      <h1>Hello, world!</h1>
-    # </div>
+#      <div class='text-center'>
+#          <h1>Hello, world!</h1>
+#     </div>
 
-    # <!-- Bootstrap core JavaScript
-    #  ================================================== -->
-    # <!-- Placed at the end of the document so the pages load faster -->
-    # <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    #    crossorigin="anonymous"></script>
-    # <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    # <script src="../../assets/js/vendor/popper.min.js"></script>
-    # <script src="../../dist/js/bootstrap.min.js"></script>
+#     <!-- Bootstrap core JavaScript
+#      ================================================== -->
+#     <!-- Placed at the end of the document so the pages load faster -->
+#     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+#        crossorigin="anonymous"></script>
+#     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+#     <script src="../../assets/js/vendor/popper.min.js"></script>
+#     <script src="../../dist/js/bootstrap.min.js"></script>
 # </body>
 
 # </html>
