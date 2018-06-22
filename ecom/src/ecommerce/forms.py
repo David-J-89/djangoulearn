cce7170b668ce583720ddf28f542e5a47afa842a
David-J-89 <davidwhaleycode@outlook.com>
2018-06-22 12:17:30 -0500

    fri04

--- ecom/src/ecommerce/forms.py
+++ ecom/src/ecommerce/forms.py
@@ -42,4 +42,4 @@ class RegisterForm(forms.Form):
     username = forms.CharField()
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password'widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
