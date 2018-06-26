0e2b7eedd81f6ce8f1f698c42cc20f4ed7f95453
David-J-89 <davidwhaleycode@outlook.com>
2018-06-26 16:11:44 -0500

    62606

--- ecom/src/products/views.py
+++ ecom/src/products/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
 from .models import Product
@@ -21,3 +21,24 @@ def product_list_view(request):
         'object_list': queryset
     }
     return render(request, "products/list.html", context)
+
+
+
+    #class-based view
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
+
+#function-based view
+def product_detail_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "products/detail.html", context)
