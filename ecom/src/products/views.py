238ae798580ff9b005403a3db45498f02993a754
David-J-89 <davidwhaleycode@outlook.com>
2018-06-26 13:07:42 -0500

    62604

--- ecom/src/products/views.py
+++ ecom/src/products/views.py
@@ -1,4 +1,17 @@
-from django.views.generic.list import ListView
+from django.views.generic import ListView
 from django.shortcuts import render
 
 from .models import Product
+
+#class-based view
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+
+
+#function-based view
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "product/product_list_view.html", context)
