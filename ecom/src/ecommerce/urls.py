5439581de6f52d7d6d3d562e0696d4a9bc4752de
David-J-89 <davidwhaleycode@outlook.com>
2018-06-27 09:58:24 -0500

    wed2702

--- ecom/src/ecommerce/urls.py
+++ ecom/src/ecommerce/urls.py
@@ -40,7 +40,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatters = urlpatterns + \
+    urlpatterns = urlpatterns + \
         static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns = urlpatterns + \
         static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
