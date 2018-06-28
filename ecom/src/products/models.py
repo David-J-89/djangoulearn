b845ddfcb787d93b051f72b517a1e2d3ef6dc687
David-J-89 <davidwhaleycode@outlook.com>
2018-06-28 13:36:39 -0500

    062804

--- ecom/src/products/models.py
+++ ecom/src/products/models.py
@@ -27,12 +27,18 @@ class ProductManager(models.Manager):
        return None
 
 class Product(models.Model):  # make sure to name your models as singular
-    title = models.CharField(max_length=120)
+    title       = models.CharField(max_length=120)
     description = models.TextField()
-    price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
-    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    price       = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+    image       = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    featured    = models.BooleanField(default=False) 
 
     objects = ProductManager()
 
+    #current way 
     def __str__(self):
+        return self.title
+        
+    #old way, I think?
+    def __unicode__(self):
         return self.title
