be4070e05ee738ab8213689cb59ffa40a74d8df6
David-J-89 <davidwhaleycode@outlook.com>
2018-07-02 14:43:54 -0500

    070207

--- ecom/src/products/models.py
+++ ecom/src/products/models.py
@@ -1,6 +1,9 @@
 import random
 import os
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+
+
 from .utils import unique_slug_generator
 
 # Create your models here.
@@ -61,3 +64,11 @@ class Product(models.Model):  # make sure to name your models as singular
     #old way, I think?
     def __unicode__(self):
         return self.title
+
+
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug=unique_slug_generator(instance)
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
