f3f4c4056ffaf33cbcaf2b7527a176fc4570d2a1
David-J-89 <davidwhaleycode@outlook.com>
2018-07-05 13:22:56 -0500

    070501

--- calendar/calendarproject/calendarproject/urls.py
+++ calendar/calendarproject/calendarproject/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from myapp import views
@@ -20,4 +23,8 @@ from myapp import views
 urlpatterns = [
     path('', views.index),
     path('admin/', admin.site.urls),
-]
+] 
+if settings.DEBUG:
+    urlpatterns = urlpatterns + \
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
