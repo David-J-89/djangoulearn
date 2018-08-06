01cd28f1baa4890488a55bb80945d8994a259093
David-J-89 <davidwhaleycode@outlook.com>
2018-08-06 09:43:43 -0500

    080606

--- rssreader/rssreader/rss/tests.py
+++ rssreader/rssreader/rss/tests.py
@@ -36,78 +36,78 @@ class RssFeedModelTests(TestCase):
         )
 
 
-class RssRestFeedsViewTests(TestCase):
-    def test_create_feed(self):
-        url = "https://www.djangoprject.com/rss/weblog/"
-        json_data = json.dumps({"url": url})
+# class RssRestFeedsViewTests(TestCase):
+#     def test_create_feed(self):
+#         url = "https://www.djangoprject.com/rss/weblog/"
+#         json_data = json.dumps({"url": url})
 
-        response = self.client.post(
-            reverse("rest-feeds"),
-            json_data,
-            content_type="application/json"
-        )
+#         response = self.client.post(
+#             reverse("rest-feeds"),
+#             json_data,
+#             content_type="application/json"
+#         )
 
-        feeds = Feed.objects.all()
+#         feeds = Feed.objects.all()
 
-        self.assertEqual(response.status_code, 201)
-        self.assertQuerysetEqual(
-            feeds,
-            ["<Feed '{}'>".format(url)]
-        )
+#         self.assertEqual(response.status_code, 201)
+#         self.assertQuerysetEqual(
+#             feeds,
+#             ["<Feed '{}'>".format(url)]
+#         )
 
-    def test_get_feeds(self):
-        url = "https://www.djangoproject.com/rss/weblog/"
+#     def test_get_feeds(self):
+#         url = "https://www.djangoproject.com/rss/weblog/"
 
-        Feed.objects.create(
-            url=url
-        )
+#         Feed.objects.create(
+#             url=url
+#         )
 
-        response = self.client.get(reverse('rest-feeds'))
-        feed = response.json()[0]
+#         response = self.client.get(reverse('rest-feeds'))
+#         feed = response.json()[0]
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(feed["url"], url)
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(feed["url"], url)
 
-    def test_update_feed(self):
-        url = "https://www.djangoproject.com/rss/weblog/"
-        new_url = "https://utan.io/?feed=rss2"
+#     def test_update_feed(self):
+#         url = "https://www.djangoproject.com/rss/weblog/"
+#         new_url = "https://utan.io/?feed=rss2"
 
-        Feed.objects.create(
-            url=url
-        )
+#         Feed.objects.create(
+#             url=url
+#         )
 
-        json_data = json.dumps({
-            "url": new_url        
-        })
+#         json_data = json.dumps({
+#             "url": new_url        
+#         })
 
-        response = self.client.put(
-            "/rss/feeds/1/",
-            json_data,
-            content_type="application/json"
-        )
+#         response = self.client.put(
+#             "/rss/feeds/1/",
+#             json_data,
+#             content_type="application/json"
+#         )
 
-        feeds = Feed.objects.all()
+#         feeds = Feed.objects.all()
 
-        self.assertEqual(response.status_code, 200)
-        self.assertQuerysetEqual(
-            feeds,
-            ["<Feed '{}'>".format(new_url)]
-        )
+#         self.assertEqual(response.status_code, 200)
+#         self.assertQuerysetEqual(
+#             feeds,
+#             ["<Feed '{}'>".format(new_url)]
+#         )
 
-    def test_delete_feed(self):
-        Feed.objects.create(
-            url="https://www.djangoproject.com/rss/weblog/"
-        )
+#     def test_delete_feed(self):
+#         Feed.objects.create(
+#             url="https://www.djangoproject.com/rss/weblog/"
+#         )
 
-        response = self.client.delete("/rss/feeds/1/")
+#         response = self.client.delete("/rss/feeds/1/")
 
-        feeds = Feed.objects.all()
+#         feeds = Feed.objects.all()
 
-        self.assertEqual(response.status_code, 200)
-        self.assertQuerysetEqual(
-            feeds,
-            []
-        )
+#         self.assertEqual(response.status_code, 200)
+#         self.assertQuerysetEqual(
+#             feeds,
+#             []
+#         )
 
 class RssRestItemsViewTests(TestCase):
 
--- rssreader/rssreader/rss/views.py
+++ rssreader/rssreader/rss/views.py
@@ -11,24 +11,7 @@ from .models import Feed
 # Create your views here.
 
 def index(request):
-
-    # url = 'https://www.djangoproject.com/rss/weblog/'
-    # feed = feedparser.parse(url)
-
-    # return render(request, 'rss/reader.html', {
-    #     'feed': feed
-    # })
-
-
-    if request.GET.get("url"):
-        url = request.GET["url"]
-        feed = feedparser.parse(url)
-    else:
-        feed = None
-
-    return render(request, 'rss/reader.html', {
-        'feed': feed
-    })
+    return render(request, 'rss/reader.html')
 
 @csrf_exempt
 def rest_feeds(request):
