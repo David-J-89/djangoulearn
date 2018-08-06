ddb9507d0323ce4782570590f54321c85d60a6d6
David-J-89 <davidwhaleycode@outlook.com>
2018-08-06 09:37:51 -0500

    080603

--- rssreader/rssreader/rss/views.py
+++ rssreader/rssreader/rss/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import feedparser
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .serializers import FeedSerializer
+from .models import Feed
 
 # Create your views here.
 
@@ -23,3 +29,47 @@ def index(request):
     return render(request, 'rss/reader.html', {
         'feed': feed
     })
+
+@csrf_exempt
+def rest_feeds(request):
+    if request.method == "GET":
+        feeds = Feed.objects.all()
+        serializer = FeedSerializer(feeds, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = FeedSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+
+        return JsonResponse(serializer.errors, status=400)
+        
+
+
+@csrf_exempt
+def rest_feeds_detail(request, pk):
+        try:
+                feed = Feed.objects.get(pk=pk)
+        except Feed.DoesNotExist:
+                return HttpResponse(status=404)
+
+        if request.method == "GET":
+                serializer = FeedSerializer(feed)
+                return JsonResponse(serializer.data)
+
+        elif request.method == "PUT":
+                data = JSONParser().parse(request)
+                serializer = FeedSerializer(feed, data=data)
+
+                if serializer.is_valid():
+                        serializer.save()
+                        return JsonResponse(serializer.data)
+
+                return JsonResponse(serializer.errors, status=400)
+
+        elif request.method == "DELETE":
+                feed.delete()
+                return HttpResponse()
