5aec917804724955039d036c3e352e3678b94c0b
David-J-89 <davidwhaleycode@outlook.com>
2018-08-07 12:49:19 -0500

    080703

--- dashboard/dashboard/dashboard/notepad/views.py
+++ dashboard/dashboard/dashboard/notepad/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Note
+from .forms import NoteModelForm
 # Create your views here.
 
 #CRUD
@@ -7,3 +8,15 @@ from .models import Note
 
 def create_view(request):
     
+    form = NoteModelForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.instance.user = request.user
+        form.save()
+        return redirect('/')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "notepad/create.html", context)
+
