85f23a3bfe300070cceac6e0118cdb4d16c12528
David-J-89 <davidwhaleycode@outlook.com>
2018-08-09 09:03:45 -0500

    080904

--- dashboard/src/news/views.py
+++ dashboard/src/news/views.py
@@ -9,4 +9,9 @@ def scrape():
     session = requests.Session()
     session.headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36"}
     url = 'https://www.theonion.com/'
+
     content = session.get(url, verify=False).content
+
+    soup = BeautifulSoup(content, "html.parser")
+
+    columns = soup.find_all('div', {'class':'curation-module__zone grid__zone'})
