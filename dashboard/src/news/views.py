24aebe7f20c055c27bb230a32ce271a166090c8c
David-J-89 <davidwhaleycode@outlook.com>
2018-08-09 09:00:31 -0500

    080903

--- dashboard/src/news/views.py
+++ dashboard/src/news/views.py
@@ -8,3 +8,5 @@ from bs4 import BeautifulSoup
 def scrape():
     session = requests.Session()
     session.headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36"}
+    url = 'https://www.theonion.com/'
+    content = session.get(url, verify=False).content
